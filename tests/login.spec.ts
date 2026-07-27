68e5d54e206edca2d0427472ed81541f43efe415
Katherine Kamenova <katherine.kamenova@example.com>
2026-07-27 14:15:07 +0200

    adding_more_tests

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -9,19 +9,11 @@ test.describe('Authentication flow', () => {
     await loginPage.open();
     await loginPage.loginWithValidCredentials(Credentials.validUser);
 
-    await expect(page).toHaveURL('https://practicesoftwaretesting.com/');
+    await expect(page).toHaveURL('https://practicesoftwaretesting.com/account');
   });
 });
 
 test.describe('Unsuccessful login attempts', () => {
-//   test('Invalid credentials show an error message', { tag: ['@regression'] }, async ({ page }) => {
-//     const loginPage = new LoginPage(page);
-
-//     await loginPage.open();
-//     await loginPage.loginWithInvalidCredentials(Credentials.invalidUser);
-//     await expect(page.getByText('Email is required', { exact: true })).toBeVisible();
-//   });
-
   test('Empty credentials show an error message', { tag: ['@regression'] }, async ({ page }) => {
     const loginPage = new LoginPage(page);
 
