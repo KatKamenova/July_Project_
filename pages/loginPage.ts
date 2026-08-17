3ad1f4d4abcccad2f043de2f1e9f21f0572cdf38
Katherine Kamenova <katherine.kamenova@example.com>
2026-08-17 11:35:19 +0200

    debugging

--- pages/loginPage.ts
+++ pages/loginPage.ts
@@ -19,9 +19,7 @@ class LoginPage {
 
   async open(): Promise<void> {
     await this.page.goto('/auth/login');
-    console.log('Immediately:', await this.emailInput.count());
     await this.emailInput.waitFor({ state: 'visible' });
-    console.log('After 1 sec:', await this.emailInput.count());
   }
 
   async loginWithValidCredentials({ emailAddress, password }: { emailAddress: string; password: string }): Promise<void> {
